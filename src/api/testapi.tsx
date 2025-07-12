1bb7439c7350596aa66bb751acf60c9a3c93ca99
symien moore <symi@symiens-MacBook-Air.local>
2025-07-11 20:58:01 -0400

    small change

---
+++ src/api/testapi.tsx
@@ -0,0 +1,19 @@
+
+
+const TestApi = () => {
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TestApi
